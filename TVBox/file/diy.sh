e3638be0854725c5d6ecd276eb3139651f36e6b9
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-05-24 20:22:44 +0800

    Update diy.sh

--- TVBox/file/diy.sh
+++ TVBox/file/diy.sh
@@ -11,7 +11,7 @@ echo '软件名称修改完成'
 # 添加播放源
 sed -i '/tv.api.config.LiveConfig/a\import com.fongmi.android.tv.bean.Config;' app/src/main/java/com/fongmi/android/tv/App.java   # import添加播放源Config
 sed -i '/this.activity = activity/a\        Config.create(0,"https://gitee.com/MINGERTAI/tvbox-json/raw/main/tv/0/0821.json").update();' app/src/main/java/com/fongmi/android/tv/App.java   # 默认点播配置
-echo '添加默认点播配置完成
+echo '添加默认点播配置完成'
 sed -i '/默认点播配置/a\        Config.create(1,"https://gitee.com/MINGERTAI/tvbox-json/raw/main/tv/0/0821.json").update();' app/src/main/java/com/fongmi/android/tv/App.java   # 默认直播配置
 echo '添加默认直播配置完成'
 
@@ -32,4 +32,4 @@ echo '修改APK LOGO完成'
 # 更改扫码管理页
 cp -rf ${{ github.workspace }}/code/TVBox/file/index.html app/src/main/assets/app/src/main/assets/index.html
 # sed -i 's/影視/QTM影视/g' app/src/main/assets/index.html
-echo '修改远程管理首页名完成
+echo '修改远程管理首页名完成'
