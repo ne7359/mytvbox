18fffdfcfcf3d0ce6b0c36d0eb1265a274cf0a46
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-05-31 14:53:30 +0800

    Update data.sh

--- TVBox/data.sh
+++ TVBox/data.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-sed -i '/proguardFiles getDefaultProguardFile/a\            signingConfig signingConfigs.debug' app/build.gradle
-sed -i '/ijkplayer/a\    implementation project('\'':pyramid'\'')' app/build.gradle
-sed -i 's/requests/requests==2.24.0/g' pyramid/build.gradle
-sed -i '/pycryptodome/a\                install '\''soupsieve==2.4'\''' pyramid/build.gradle
-sed -i '/ijkplayer/a\include '\''':pyramid''\''' settings.gradle
-echo '添加编译PY完成'
-
 sed -i '/Updater.get().release().start(this)/d' app/src/mobile/java/com/fongmi/android/tv/ui/activity/MainActivity.java
 sed -i '/Updater.get().release().start(this)/d' app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
 echo '删除自动更新完成'
